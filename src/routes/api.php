50e379c29add2e311ff8d3a9dcc7a0ab61112540
Ilja Suhinin <iljasuhinin@gmail.com>
2024-07-05 12:00:25 +0300

    update route name

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Packages\Reporter\Http\Controllers\ReportController;
 
-Route::prefix('/api/reporter')->group(function () {
+Route::prefix('/api/report')->group(function () {
     Route::any('/', [ReportController::class, 'report']);
 });
